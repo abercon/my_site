5960a5cce13057262d134ef06ed2a13cd3c5a7e3
Joshua <joshua@beamery.com>
2018-07-11 22:38:51 +0100

    inital commit

---
+++ src/app/welcome-page/feed/feed-body/feed-body.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'my-site-feed-body',
+  templateUrl: './feed-body.component.html',
+  styleUrls: ['./feed-body.component.scss']
+})
+export class FeedBodyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
